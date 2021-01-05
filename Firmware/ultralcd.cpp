d45f53f3772b8ea055c81c48d0cd5fae2000eeb0
valère didon <vdidon@live.fr>
2021-01-05 13:36:00 +0100

    Add to setting : fast home and fan speed

--- Firmware/ultralcd.cpp
+++ Firmware/ultralcd.cpp
@@ -5732,6 +5732,8 @@ static void lcd_settings_menu() {
     if (!isPrintPaused)
       MENU_ITEM_GCODE_P(_i("Disable steppers"), PSTR("M84"));////MSG_DISABLE_STEPPERS
 
+    MENU_ITEM_EDIT_int3_P(_T(MSG_FAN_SPEED), &fanSpeed, 0, 255);
+    MENU_ITEM_GCODE_P(_N("Fast home"), PSTR("G28 W"));////MSG_DISABLE_STEPPERS
     MENU_ITEM_SUBMENU_P(_T(MSG_BABYSTEP_Z), lcd_babystep_z);
 
     SETTINGS_FILAMENT_SENSOR;

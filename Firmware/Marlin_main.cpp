vdidon/Prusa-Firmware

commit 3957dc91aa0f1b592071ec7728b156e3b62c54bc

Add commit number and hash to `M115` Don't show UUID only if defined

Firmware/Marlin_main.cpp
@@ -1121,7 +1121,7 @@ void setup()
 		SERIAL_PROTOCOLLNPGM("start");
 #endif
 	SERIAL_ECHO_START;
-	puts_P(PSTR(" " FW_VERSION_FULL));
+	puts_P(PSTR(" " FW_VERSION_FULL "_" FW_COMMIT_HASH));
 
 	// by default the MMU shall remain disabled - PFW-1418
 	if (eeprom_init_default_byte((uint8_t *)EEPROM_MMU_ENABLED, 0)) {
@@ -6511,13 +6511,19 @@ Sigma_Exit:
           eeprom_read_block(custom_mendel_name,(char*)EEPROM_CUSTOM_MENDEL_NAME,MAX_CUSTOM_MENDEL_NAME_LENGTH);
           SERIAL_ECHOPGM("FIRMWARE_NAME:Prusa-Firmware ");
           SERIAL_ECHORPGM(FW_VERSION_STR_P());
+          SERIAL_ECHOPGM("+");
+          SERIAL_ECHOPGM(STR(FW_COMMITNR));
+          SERIAL_ECHOPGM("_");
+          SERIAL_ECHOPGM(FW_COMMIT_HASH);
           SERIAL_ECHOPGM(" based on Marlin FIRMWARE_URL:https://github.com/prusa3d/Prusa-Firmware PROTOCOL_VERSION:");
           SERIAL_ECHOPGM(PROTOCOL_VERSION);
           SERIAL_ECHOPGM(" MACHINE_TYPE:");
           SERIAL_PROTOCOL(custom_mendel_name);
           SERIAL_ECHOPGM(" EXTRUDER_COUNT:" STRINGIFY(EXTRUDERS));
+#ifdef MACHINE_UUID
           SERIAL_ECHOPGM(" UUID:");
           SERIAL_ECHOLNPGM(MACHINE_UUID);
+#endif //MACHINE_UUID
 #ifdef EXTENDED_CAPABILITIES_REPORT
           extended_capabilities_report();
 #endif //EXTENDED_CAPABILITIES_REPORT

Firmware/Configuration.h
@@ -34,7 +34,7 @@ extern const char _sPrinterMmuName[] PROGMEM;
     // Construct the TWEAK value as it is expected from the enum.
     #define FW_TWEAK (CONCAT(FIRMWARE_REVISION_,FW_FLAVOR) + FW_FLAVERSION)
     #define FW_VERSION STR(FW_MAJOR) "." STR(FW_MINOR) "." STR(FW_REVISION) "-" STR(FW_FLAVOR) "" STR(FW_FLAVERSION)
-    #define FW_VERSION_FULL STR(FW_MAJOR) "." STR(FW_MINOR) "." STR(FW_REVISION) "-" STR(FW_FLAVOR) "" STR(FW_FLAVERSION) "-" STR(FW_COMMITNR)
+    #define FW_VERSION_FULL STR(FW_MAJOR) "." STR(FW_MINOR) "." STR(FW_REVISION) "-" STR(FW_FLAVOR) "" STR(FW_FLAVERSION) "+" STR(FW_COMMITNR)
 #endif
 
 // The full version string and repository source are set via cmake
@@ -70,7 +70,7 @@ extern const char _sPrinterMmuName[] PROGMEM;
 // build by the user have been successfully uploaded into firmware.
 
 #define STRING_VERSION_CONFIG_H SOURCE_DATE_EPOCH " " SOURCE_TIME_EPOCH // build date and time
-#define STRING_CONFIG_H_AUTHOR "(none, default config)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR FW_REPOSITORY // Who made the changes.
 
 // SERIAL_PORT selects which serial port should be used for communication with the host.
 // This allows the connection of wireless adapters (for instance) to non-default port pins.

Firmware/language.h
@@ -13,10 +13,6 @@
 
 #define PROTOCOL_VERSION "1.0"
 
-#ifndef MACHINE_UUID
-   #define MACHINE_UUID "00000000-0000-0000-0000-000000000000"
-#endif
-
 #if (LANG_MODE == 0) //primary language only
 #define PROGMEM_I2 __attribute__((section(".progmem0")))
 #define PROGMEM_I1 __attribute__((section(".progmem1")))

commit 51b119a3ec729971a66e3ac563fb9efc72b76698

Fix Autoreport broken in https://github.com/prusa3d/Prusa-Firmware/commit/3957dc91aa0f1b592071ec7728b156e3b62c54bc

Firmware/Marlin_main.cpp
@@ -6575,8 +6575,9 @@ Sigma_Exit:
           SERIAL_ECHOPGM(" EXTRUDER_COUNT:" STRINGIFY(EXTRUDERS));
 #ifdef MACHINE_UUID
           SERIAL_ECHOPGM(" UUID:");
-          SERIAL_ECHOLNPGM(MACHINE_UUID);
+          SERIAL_ECHOPGM(MACHINE_UUID);
 #endif //MACHINE_UUID
+          SERIAL_ECHOLNPGM("");
 #ifdef EXTENDED_CAPABILITIES_REPORT
           extended_capabilities_report();
 #endif //EXTENDED_CAPABILITIES_REPORT
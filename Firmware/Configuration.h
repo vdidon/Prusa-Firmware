ab62030c7f96e8fe9a3f5ba161a4b8a09547e9e0
3d-gussner <3d.gussner@gmail.com>
2024-09-24 16:50:14 +0200

    Bump up build number and final version

--- Firmware/Configuration.h
+++ Firmware/Configuration.h
@@ -22,9 +22,9 @@ extern const char _sPrinterMmuName[] PROGMEM;
 #define FW_MAJOR 3
 #define FW_MINOR 14
 #define FW_REVISION 1
-#define FW_COMMITNR 8225
-#define FW_FLAVOR RC      //uncomment if DEV, ALPHA, BETA or RC
-#define FW_FLAVERSION 1     //uncomment if FW_FLAVOR is defined and versioning is needed. Limited to max 8.
+#define FW_COMMITNR 8237
+//#define FW_FLAVOR RC      //uncomment if DEV, ALPHA, BETA or RC
+//#define FW_FLAVERSION 1     //uncomment if FW_FLAVOR is defined and versioning is needed. Limited to max 8.
 #endif
 
 #ifndef FW_FLAVOR
